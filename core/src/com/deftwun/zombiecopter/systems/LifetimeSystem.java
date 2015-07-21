60d4cd721e2781f34ac48d51cd870163d8accec1
DaanVanYperen <daan@mostlyoriginal.net>
2015-07-21 13:15:45 +0200

    Migrate LifetimeSystem.java

--- core/src/com/deftwun/zombiecopter/systems/LifetimeSystem.java
+++ core/src/com/deftwun/zombiecopter/systems/LifetimeSystem.java
@@ -1,34 +1,34 @@
 package com.deftwun.zombiecopter.systems;
 
-import com.badlogic.ashley.core.Entity;
-import com.badlogic.ashley.core.Family;
-import com.badlogic.ashley.systems.IteratingSystem;
+import com.artemis.Aspect;
+import com.artemis.Entity;
+import com.artemis.systems.EntityProcessingSystem;
 import com.badlogic.gdx.utils.Logger;
-import com.deftwun.zombiecopter.ComponentMappers;
 import com.deftwun.zombiecopter.App;
+import com.deftwun.zombiecopter.ComponentMappers;
 import com.deftwun.zombiecopter.components.HealthComponent;
 import com.deftwun.zombiecopter.components.PhysicsComponent;
 import com.deftwun.zombiecopter.components.TimeToLiveComponent;
 
-public class LifetimeSystem extends IteratingSystem{
+public class LifetimeSystem extends EntityProcessingSystem {
 	private Logger logger = new Logger("LifeTimeSystem",Logger.INFO);
 	
 	@SuppressWarnings("unchecked")
 	public LifetimeSystem() {
-		super(Family.all(PhysicsComponent.class)
-				    .one(HealthComponent.class,TimeToLiveComponent.class).get());
+		super(Aspect.all(PhysicsComponent.class)
+				    .one(HealthComponent.class,TimeToLiveComponent.class));
 		logger.debug("initializing");
 	}		
 
 	@Override
-	protected void processEntity(Entity entity, float deltaTime) {
+	protected void process(Entity entity) {
 		
 		ComponentMappers mappers = App.engine.mappers;
 		TimeToLiveComponent timeToLive = mappers.timeToLive.get(entity);
 	
 		//Time to live (expiration)
 		if (timeToLive != null){
-			timeToLive.time += deltaTime;
+			timeToLive.time += world.delta;
 			if (timeToLive.time >= timeToLive.timeLimit){
 				logger.debug("Entity #" + entity.getId() + " has expired: Time to live exceeded");
 				App.engine.removeEntity(entity);
