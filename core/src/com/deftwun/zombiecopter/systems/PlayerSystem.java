8d945cb4089bf2c2989b4b905b17908b18c64f08
DaanVanYperen <daan@mostlyoriginal.net>
2015-07-21 13:20:37 +0200

    Migrate PlayerSystem.java

--- core/src/com/deftwun/zombiecopter/systems/PlayerSystem.java
+++ core/src/com/deftwun/zombiecopter/systems/PlayerSystem.java
@@ -1,21 +1,20 @@
 package com.deftwun.zombiecopter.systems;
 
-import com.badlogic.ashley.core.Entity;
-import com.badlogic.ashley.core.EntityListener;
-import com.badlogic.ashley.core.EntitySystem;
+import com.artemis.BaseSystem;
+import com.artemis.Entity;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Logger;
-import com.deftwun.zombiecopter.ComponentMappers;
 import com.deftwun.zombiecopter.App;
+import com.deftwun.zombiecopter.ComponentMappers;
 import com.deftwun.zombiecopter.UserInterface;
 import com.deftwun.zombiecopter.components.ControllerComponent;
 import com.deftwun.zombiecopter.components.PhysicsComponent;
 import com.deftwun.zombiecopter.components.VehicleComponent;
 import com.deftwun.zombiecopter.components.VehicleOperatorComponent;
 
-public class PlayerSystem extends EntitySystem implements EntityListener,InputProcessor{	
+public class PlayerSystem extends BaseSystem implements InputProcessor {
 	private Logger logger = new Logger("PlayerSystem",Logger.INFO);	
 	private Entity currentPlayer = null;
 	
@@ -35,7 +34,7 @@ public class PlayerSystem extends EntitySystem implements EntityListener,InputPr
 	}
 	
 	@Override
-	public void update(float deltaTime) {
+	public void processSystem() {
 
 		if (currentPlayer == null) return;
 		ComponentMappers mappers = App.engine.mappers;
@@ -63,23 +62,8 @@ public class PlayerSystem extends EntitySystem implements EntityListener,InputPr
 		
 	}
 
-	@Override
-	public void entityAdded(Entity entity) {
-		//If an current player is not set and a new entity is added with a player component,
-		// then set the new entity as the current player
-		/*
-		if (currentPlayer == null){
-			if (Platformer.engine.mappers.player.has(entity)){
-				logger.debug("Player set to entity #" + entity.getId());
-				Platformer.engine.systems.camera.setFollow(entity);
-				currentPlayer = entity;
-			}
-		}
-		*/
-	}
-
-	@Override
-	public void entityRemoved(Entity entity) {
+	//@todo migrate
+	public void removed(Entity entity) {
 		//If the current player is removed then current player = null;
 		if (currentPlayer == entity){
 			logger.debug("Current player has been removed: Entity#" + currentPlayer.getId());
