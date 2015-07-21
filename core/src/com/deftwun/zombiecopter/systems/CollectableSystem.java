cb5d99a6d304bf0de9bee09d3c4f0495f901ce0d
DaanVanYperen <daan@mostlyoriginal.net>
2015-07-21 13:42:19 +0200

    Migrate CollectableSystem.java

--- core/src/com/deftwun/zombiecopter/systems/CollectableSystem.java
+++ core/src/com/deftwun/zombiecopter/systems/CollectableSystem.java
@@ -1,31 +1,42 @@
 package com.deftwun.zombiecopter.systems;
 
-import com.badlogic.ashley.core.Entity;
-import com.badlogic.ashley.core.Family;
-import com.badlogic.ashley.systems.IteratingSystem;
-import com.deftwun.zombiecopter.ComponentMappers;
+import com.artemis.Aspect;
+import com.artemis.Entity;
+import com.artemis.systems.EntityProcessingSystem;
+import com.artemis.utils.IntBag;
 import com.deftwun.zombiecopter.App;
+import com.deftwun.zombiecopter.ComponentMappers;
 import com.deftwun.zombiecopter.components.Collectable;
 import com.deftwun.zombiecopter.components.Collector;
 import com.deftwun.zombiecopter.components.PhysicsComponent;
 
-public class CollectableSystem extends IteratingSystem {
-	
+public class CollectableSystem extends EntityProcessingSystem {
+
+	private Entity collectableFly;
+
 	@SuppressWarnings("unchecked")
 	public CollectableSystem() {
-		super(Family.all(PhysicsComponent.class,Collector.class).get());
+		super(Aspect.all(PhysicsComponent.class, Collector.class));
 	}
+
+	@Override
+	protected void initialize() {
+		super.initialize();
+		collectableFly = createFlyweightEntity();
+}
 
 	@SuppressWarnings("unchecked")
 	@Override
-	protected void processEntity(Entity entity, float deltaTime) {
+	protected void process(Entity entity) {
 		ComponentMappers maps =App.engine.mappers;
 		PhysicsComponent physics = maps.physics.get(entity);
 		Collector collector = maps.collector.get(entity);
-		
-		for (Entity e : App.engine.getEntitiesFor(Family.all(Collectable.class).get())){
-			Collectable c = maps.collectable.get(e);
-			PhysicsComponent p = maps.physics.get(e);
+
+		final IntBag collectables = App.engine.getEntitiesFor(Aspect.all(Collectable.class));
+		for (int i = 0, s = collectables.size(); i < s; i++) {
+			collectableFly.id = collectables.get(i);
+			Collectable c = maps.collectable.get(collectableFly);
+			PhysicsComponent p = maps.physics.get(collectableFly);
 			if (c!= null && p != null){
 				if (p.getPosition().sub(physics.getPosition()).len() < c.pickupRange){
 					if (c.type == Collectable.ItemType.Civilian && collector.civilians < collector.maxCivilians) {
